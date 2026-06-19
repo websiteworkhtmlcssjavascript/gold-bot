b90ccfef87488560802589ceab27542252148a64
Abdou_19 <142795203+websiteworkhtmlcssjavascript@users.noreply.github.com>
2026-06-19 10:24:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import asyncio
 import requests
 from telegram import Bot
 
-BOT_TOKEN = os.getenv("8826488858:AAFLXXaWXnNX00LdJuXT9QweAy1dXTFpt-8")
-GOLD_API_KEY = os.getenv("goldapi-315b0b99d2ec5744f99b0c37edaf77cb-io")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GOLD_API_KEY = os.getenv("GOLD_API_KEY")
 
 CHANNEL_ID = "@FRXEXNSSMT5"
 
@@ -12,43 +12,44 @@ bot = Bot(token=BOT_TOKEN)
 
 message_id = None
 
+
 async def update_gold():
-global message_id
+    global message_id
 
-while True:
-    try:
-        headers = {
-            "x-access-token": GOLD_API_KEY
-        }
+    while True:
+        try:
+            headers = {
+                "x-access-token": GOLD_API_KEY
+            }
 
-        response = requests.get(
-            "https://www.goldapi.io/api/XAU/USD",
-            headers=headers,
-            timeout=10
-        )
-
-        data = response.json()
-
-        price = data.get("price", "N/A")
-
-        text = f"🟡 Gold Price\n\nXAU/USD: {price} USD"
-
-        if message_id is None:
-            msg = await bot.send_message(
-                chat_id=CHANNEL_ID,
-                text=text
-            )
-            message_id = msg.message_id
-        else:
-            await bot.edit_message_text(
-                chat_id=CHANNEL_ID,
-                message_id=message_id,
-                text=text
+            response = requests.get(
+                "https://www.goldapi.io/api/XAU/USD",
+                headers=headers,
+                timeout=10
             )
 
-    except Exception as e:
-        print("ERROR:", e)
+            data = response.json()
+            price = data.get("price", "N/A")
 
-    await asyncio.sleep(5)
+            text = f"🟡 Gold Price\n\nXAU/USD: {price} USD"
+
+            if message_id is None:
+                msg = await bot.send_message(
+                    chat_id=CHANNEL_ID,
+                    text=text
+                )
+                message_id = msg.message_id
+            else:
+                await bot.edit_message_text(
+                    chat_id=CHANNEL_ID,
+                    message_id=message_id,
+                    text=text
+                )
+
+        except Exception as e:
+            print("ERROR:", e)
+
+        await asyncio.sleep(5)
+
 
 asyncio.run(update_gold())
