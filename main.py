1965f7c124efa6892bdd870d9e7cc9f6b8e2ccf9
Abdou_19 <142795203+websiteworkhtmlcssjavascript@users.noreply.github.com>
2026-06-19 11:12:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,11 @@
-import os
-import asyncio
-import requests
-from telegram import Bot
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-GOLD_API_KEY = os.getenv("GOLD_API_KEY")
-
-CHANNEL_ID = "@gold_price_live_2026"
-
-bot = Bot(token=BOT_TOKEN)
-
-message_id = None
-
+last_text = ""
 
 async def update_gold():
-    global message_id
+    global message_id, last_text
 
     while True:
         try:
-            headers = {
-                "x-access-token": GOLD_API_KEY,
-                "Content-Type": "application/json"
-            }
-
-            response = requests.get(
-                "https://www.goldapi.io/api/XAU/USD",
-                headers=headers,
-                timeout=10
-            )
-
-            data = response.json()
-
-            price = data.get("price")
-
+            ...
             text = f"""🟡 GOLD LIVE
 
 💰 XAU/USD: {price} USD
@@ -40,26 +13,27 @@ async def update_gold():
 🔄 Update every 5 seconds
 """
 
-            if message_id is None:
-                msg = await bot.send_message(
-                    chat_id=CHANNEL_ID,
-                    text=text
-                )
-                message_id = msg.message_id
+            if text != last_text:
 
-            else:
-                await bot.edit_message_text(
-                    chat_id=CHANNEL_ID,
-                    message_id=message_id,
-                    text=text
-                )
+                if message_id is None:
+                    msg = await bot.send_message(
+                        chat_id=CHANNEL_ID,
+                        text=text
+                    )
+                    message_id = msg.message_id
 
-            print(f"Price: {price}")
+                else:
+                    await bot.edit_message_text(
+                        chat_id=CHANNEL_ID,
+                        message_id=message_id,
+                        text=text
+                    )
+
+                last_text = text
+
+            print("Running...")
 
         except Exception as e:
             print("ERROR:", e)
 
         await asyncio.sleep(5)
-
-
-asyncio.run(update_gold())
