8dbd1da6fed1bdbbca7bf61c71853bcdb85719a3
Abdou_19 <142795203+websiteworkhtmlcssjavascript@users.noreply.github.com>
2026-06-19 11:03:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,13 @@
-import os
-import asyncio
-import requests
 from telegram import Bot
+import asyncio
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-GOLD_API_KEY = os.getenv("GOLD_API_KEY")
+BOT_TOKEN = "8826488858:AAFLXXaWXnNX00LdJuXT9QweAy1dXTFpt-8"
 
-CHANNEL_ID = "@FRXEXNSSMT5"
+async def test():
+    bot = Bot(BOT_TOKEN)
+    await bot.send_message(
+        chat_id="@gold_price_live_2026",
+        text="✅ Bot Connected"
+    )
 
-bot = Bot(token=BOT_TOKEN)
-
-message_id = None
-
-
-async def update_gold():
-    global message_id
-
-    while True:
-        try:
-            headers = {
-                "x-access-token": GOLD_API_KEY
-            }
-
-            response = requests.get(
-                "https://www.goldapi.io/api/XAU/USD",
-                headers=headers,
-                timeout=10
-            )
-
-            data = response.json()
-            price = data.get("price", "N/A")
-
-            text = f"🟡 Gold Price\n\nXAU/USD: {price} USD"
-
-            if message_id is None:
-                msg = await bot.send_message(
-                    chat_id=CHANNEL_ID,
-                    text=text
-                )
-                message_id = msg.message_id
-            else:
-                await bot.edit_message_text(
-                    chat_id=CHANNEL_ID,
-                    message_id=message_id,
-                    text=text
-                )
-
-        except Exception as e:
-            print("ERROR:", e)
-
-        await asyncio.sleep(5)
-
-
-asyncio.run(update_gold())
+asyncio.run(test())
